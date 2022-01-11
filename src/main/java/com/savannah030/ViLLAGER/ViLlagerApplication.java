038430bbcd5ed893f8ee8ea277f6db7742f45f32
savannah030 <savannah030@naver.com>
2022-01-12 06:05:52 +0900

    Initial commit

---
+++ src/main/java/com/savannah030/ViLLAGER/ViLlagerApplication.java
@@ -0,0 +1,13 @@
+package com.savannah030.ViLLAGER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViLlagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViLlagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savannah030/ViLLAGER/ViLlagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savannah030.ViLLAGER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViLlagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
