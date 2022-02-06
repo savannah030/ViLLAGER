ab0644f64a2b24ab36038ab647a22eca370c7505
savannah030 <savannah030@naver.com>
2022-02-06 15:09:34 +0900

    feat: 현재 위치와 가까운 데이터 불러오기(@Query 사용)

--- src/main/java/com/savannah030/ViLLAGER/repository/BoardRepository.java
+++ src/main/java/com/savannah030/ViLLAGER/repository/BoardRepository.java
@@ -1,11 +1,19 @@
 package com.savannah030.ViLLAGER.repository;
 
-import com.savannah030.ViLLAGER.domain.Board;
+import com.savannah030.ViLLAGER.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Board findByLatitude(double latitude);
+    Board findByLatitude(double latitude); //JpaMappingTest용
     Board findByLongitude(double longitude);
+
+    //here
+    @Query("select b from Board b where b.latitude<=:latitude+0.001*10 and b.longitude<=:longitude+0.001*10")
+    List<Board> findAllByLatitudeAndLongitude(@Param("latitude")Double latitude,@Param("longitude")Double longitude);
 }
