a746667dfe43a29fd7ff52a0d6f0325211884dab
savannah030 <savannah030@naver.com>
2022-03-07 00:52:30 +0900

    feat: Chatmessage 엔티티 추가

---
+++ src/main/java/com/savannah030/ViLLAGER/domain/entity/Chatmessage.java
@@ -0,0 +1,33 @@
+package com.savannah030.ViLLAGER.domain.entity;
+
+import com.savannah030.ViLLAGER.domain.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/*
+    채팅 메세지도 DB에 저장하기 위해서 엔티티 따로만듦
+ */
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class Chatmessage extends BaseEntity {
+
+    enum MessageType {
+        ENTER, TALK
+    }
+
+    @Id
+    @Column(name = "chatmessage_idx")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    private MessageType messageType;
+    private String message;
+
+    @ManyToOne
+    @JoinColumn(name = "chatroom_idx")
+    private Chatroom chatroom;
+}
