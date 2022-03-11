e10d75248ced684dc11438475c9a07cbc715b357
savannah030 <savannah030@naver.com>
2022-03-11 22:41:38 +0900

    feat: HomeController @GetMapping("/error-test") 추가
    사용자가 /error-test라는 잘못된 입력을 하면, VillagerException에게 예외전가를 해서 @ControllerAdvice의 @ExceptionHandler가 처리할수 있도록 함

--- src/main/java/com/savannah030/ViLLAGER/controller/HomeController.java
+++ src/main/java/com/savannah030/ViLLAGER/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.savannah030.ViLLAGER.controller;
 
+import com.savannah030.ViLLAGER.exception.GlobalControllerAdvice;
+import com.savannah030.ViLLAGER.exception.ReturnCode;
+import com.savannah030.ViLLAGER.exception.VillagerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    // TEST : ok
+    @GetMapping("/error-test")
+    public String globalControllerAdviceTest(){
+        log.info("GlobalControllerAdvice: {}",ReturnCode.TEST);
+        throw new VillagerException(ReturnCode.TEST);
     }
 }
