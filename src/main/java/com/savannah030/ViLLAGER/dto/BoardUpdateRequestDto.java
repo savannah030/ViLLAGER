288d5fc21d4a9cbc639ffe79db2b237049f8b0e1
savannah030 <savannah030@naver.com>
2022-03-13 20:08:24 +0900

    feat: BoardUpdateRequestDto 생성

---
+++ src/main/java/com/savannah030/ViLLAGER/dto/BoardUpdateRequestDto.java
@@ -0,0 +1,34 @@
+package com.savannah030.ViLLAGER.dto;
+
+import com.savannah030.ViLLAGER.domain.enums.CategoryType;
+import com.savannah030.ViLLAGER.domain.enums.StatusType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class BoardUpdateRequestDto {
+
+    private CategoryType categoryType;
+
+    private String title;
+
+    private String content;
+
+    private StatusType statusType;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    @Builder
+    public BoardUpdateRequestDto(CategoryType categoryType, String title, String content, StatusType statusType, Double latitude, Double longitude){
+        this.categoryType = categoryType;
+        this.title = title;
+        this.content = content;
+        this.statusType = statusType;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
