52ccf2cee2a4272ccd06970346e37677941e7bcd
savannah030 <savannah030@naver.com>
2022-03-14 14:50:36 +0900

    feat: MyBoardResponseDTO.java 생성

---
+++ src/main/java/com/savannah030/ViLLAGER/dto/MyBoardResponseDto.java
@@ -0,0 +1,49 @@
+package com.savannah030.ViLLAGER.dto;
+
+import com.savannah030.ViLLAGER.domain.entity.Board;
+import com.savannah030.ViLLAGER.domain.enums.CategoryType;
+import com.savannah030.ViLLAGER.domain.enums.StatusType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class MyBoardResponseDto {
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private Long idx;
+
+    private CategoryType categoryType;
+
+    private String title;
+
+    private String content;
+
+    private StatusType statusType;
+
+    private Long hits;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    @Builder
+    public MyBoardResponseDto(Board entity){
+        this.idx = entity.getIdx();
+        this.createdDate = entity.getCreatedDate();
+        this.updatedDate = entity.getUpdatedDate();
+        this.categoryType = entity.getCategoryType();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.statusType = entity.getStatusType();
+        this.hits = entity.getHits();
+        //this.latitude = entity.getAddress().getLatitude();
+        //this.longitude = entity.getAddress().getLongitude();
+    }
+}
