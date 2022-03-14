c2331e95c9e6b929e96a51f4154be835fd958023
savannah030 <savannah030@naver.com>
2022-03-15 02:11:55 +0900

    fix: form.js 현재 위치 못받아오는 문제 해결
    lat,lon을 콜백함수 안에서 초기화 시키면 안됨. 콜백함수 흐름 공부하기..

--- src/main/resources/static/js/app/form.js
+++ src/main/resources/static/js/app/form.js
@@ -12,85 +12,83 @@ const form = {
         });
     },
 
-    getLocation : function (){
-        let lat = 37.498095, //강남역
-            lon = 127.027610;
+    getLocation : function (callback){
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(position){
-                lat = position.coords.latitude;
-                lon = position.coords.longitude;
-            });
+            navigator.geolocation.getCurrentPosition(callback);
         } else {
-            alert("Geolocation is not supported by this browser.");
+            alert('사용자의 브라우저는 지오로케이션을 지원하지 않습니다.');
         }
-        return [lat,lon];
     },
 
+    // NOTE: 콜백함수 흐름 공부하기...
     save : function () {
+        this.getLocation(function (position){
 
-        let [lat,lon] = this.getLocation();
-        console.log("currentPos",lat,lon);
+            let lat = position.coords.latitude,
+                lon = position.coords.longitude;
 
-        const jsonData = JSON.stringify({
-            //idx: $('#board_idx').val(), // 기본 키 생성은 데이터베이스에 위임
-            categoryType: $('#board_category').val(),
-            title: $('#board_title').val(),
-            content: $('#board_content').val(),
-            // 처음 글 올릴때는 무조건 판매중 따라서 statusType 필요없음
-            latitude: lat,
-            longitude: lon,
+            const jsonData = JSON.stringify({
+                //idx: $('#board_idx').val(), // 기본 키 생성은 데이터베이스에 위임
+                categoryType: $('#board_category').val(),
+                title: $('#board_title').val(),
+                content: $('#board_content').val(),
+                // 처음 글 올릴때는 무조건 판매중 따라서 statusType 필요없음
+                latitude: lat,
+                longitude: lon,
+            });
+            console.log("jsonData: ",jsonData); //ok
+            $.ajax({
+                url: "http://localhost:8080/api/boards",
+                type: "POST",
+                data: jsonData,
+                contentType: "application/json",
+                dataType: "json",
+                success: function () {
+                    alert('저장 성공!');
+                    location.href = '/board/list';
+                },
+                error: function () {
+                    alert('저장 실패!');
+                }
+            });
         });
-        console.log("jsonData: ",jsonData); //ok
-        $.ajax({
-                    url: "http://localhost:8080/api/boards",
-                    type: "POST",
-                    data: jsonData,
-                    contentType: "application/json",
-                    dataType: "json",
-                        success: function () {
-                            alert('저장 성공!');
-                            location.href = '/board/list';
-                    },
-                    error: function () {
-                        alert('저장 실패!');
-                        console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
-                    }
-               });
+
     },
     // TODO 저장 버튼 누르고 게시글 목록으로 돌아간 다음 '뒤로 가기' 누르면 잘못된 접근입니다. 띄워줘야함
 
     update : function () {
+        this.getLocation(function (position) {
 
-        let [lat,lon] = this.getLocation();
-        console.log("currentPos",lat,lon);
+            let lat = position.coords.latitude,
+                lon = position.coords.longitude;
 
-        const jsonData = JSON.stringify({
-            categoryType: $('#board_category').val(),
-            title: $('#board_title').val(),
-            content: $('#board_content').val(),
-            statusType: $('#board_status').val(),
-            latitude: lat,
-            longitude: lon,
+            const jsonData = JSON.stringify({
+                categoryType: $('#board_category').val(),
+                title: $('#board_title').val(),
+                content: $('#board_content').val(),
+                statusType: $('#board_status').val(),
+                latitude: lat,
+                longitude: lon,
+            });
+            console.log("jsonData: ",jsonData); //ok
+            const idx = $('#board_idx').val();
+
+            $.ajax({
+                url: "http://localhost:8080/api/boards/" + idx,
+                type: "PUT",
+                data: jsonData,
+                contentType: "application/json",
+                dataType: "json",
+                success: function () {
+                    alert('수정 성공!');
+                    location.href = '/board/list';
+                },
+                error: function () {
+                    alert('수정 실패!');
+                }
+            });
         });
-
-        const idx = $('#board_idx').val();
-
-        $.ajax({
-           url: "http://localhost:8080/api/boards/" + idx,
-           type: "PUT",
-           data: jsonData,
-           contentType: "application/json",
-           dataType: "json",
-           success: function () {
-               alert('수정 성공!');
-               location.href = '/board/list';
-           },
-           error: function () {
-               alert('수정 실패!');
-           }
-       });
     },
-
     delete : function () {
         const idx = $('#board_idx').val();
 
