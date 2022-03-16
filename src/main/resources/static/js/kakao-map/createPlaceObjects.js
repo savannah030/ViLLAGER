3379bc30bbd4466f1c1b3cecf4535b48b2fc4a6c
savannah030 <savannah030@naver.com>
2022-03-16 13:44:23 +0900

    Feat: createPlaceObjects.js에 커스텀오버레이 클로저 만들기

--- src/main/resources/static/js/kakao-map/createPlaceObjects.js
+++ src/main/resources/static/js/kakao-map/createPlaceObjects.js
@@ -1,3 +1,5 @@
+
+
 ///장소 객체 초기화하는 함수
 function NewPlace(marker, customOverlay){
     this.marker = marker;
@@ -27,36 +29,44 @@ function createPlaceObjects(jsonData) {
     const jsonParse = JSON.parse(jsonData);
     // jsonParse.forEach((json.forEach( (data => console.log(data)))) ); //ok
 
+
     jsonParse.forEach( (json) => {
-        console.log("json");
+        // console.log("json"); //ok
         json.forEach((data) => {  // jsonParse.forEach((data) => {
 
             // console.log(data, typeof (data)); //ok // {idx: 191, categoryType: 'ELECTRONICS', title: '게시글191', latitude: 37.4965883, longitude: 126.9939492} 'object'
-            console.log("hi");
-            const idx = data.idx;
-            const category = data.categoryType;
-            const title = data.title;
-            const latitude = data.latitude;
-            const longitude = data.longitude;
 
-            console.log(idx); //ok
-            console.log(category); //ok
-            console.log(title); //ok
-            console.log(latitude); //ok
-            console.log(longitude); //ok
+            const idx = data.idx; //ok
+            const category = data.categoryType; //ok
+            const title = data.title; //ok
+            const content = data.content; //ok
+            const latitude = data.latitude; //ok
+            const longitude = data.longitude; //ok
 
-            /*
-            const location = new kakao.maps.LatLng(data["latitude"], data["longitude"]);
-            const markerImage = createMarkerImage(<i className="fa-solid fa-clothes-hanger"></i>, new kakao.maps.Size(30,30));
 
-            const marker = createMarker(location, markerImage);
-            const customOverlay = createCustomOverlay(location, data);
+            // console.log(idx); //ok
+            // console.log(category); //ok
+            // console.log(title); //ok
+            // console.log("content",content); //ok
+            // console.log(latitude,"json type:",typeof(latitude),37.4965883, typeof(37.4965883)); //ok 타입은 같은데..
+            // console.log(longitude); //ok
 
-            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map,marker,i,customOverlay));
-            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(customOverlay));
-            */
+            const location = new kakao.maps.LatLng(latitude,longitude);
+            console.log("idx",idx,"location",location);
+            // '<i className="fa-solid fa-clothes-hanger"></i>'
+            //const markerImage = createMarkerImage("src", new kakao.maps.Size(30,30)); //FIXME
+
+            //marker = createMarker(location, markerImage);
+            var customOverlay = createCustomOverlay(location, idx, category, title, content);
+            console.log("customOverlay",customOverlay);
+            customOverlay.setMap(map);
+            //kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map,marker,customOverlay));
+            //kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(customOverlay));
+
         });
     });
+
+
 
     /*
     const jsonMarkers = jsonParse[0]; //ok
@@ -66,7 +76,7 @@ function createPlaceObjects(jsonData) {
 
     jsonMarkers.forEach((jsonMarker) => {
 
-        const location = new kakao.maps.LatLng(jsonMarker[latitude], jsonMarker[longitude]);
+        const location = new kakao.maps.LatㅜLng(jsonMarker[latitude], jsonMarker[longitude]);
         const markerImage = createMarkerImage(<i className="fa-solid fa-clothes-hanger"></i>, new kakao.maps.Size(30,30));
 
         const marker = createMarker(location, markerImage);
@@ -82,8 +92,6 @@ function createPlaceObjects(jsonData) {
     });
      */
 
-
-
     /*
     getContent().addEventListener('mouseover', makeOverListener(map,marker,i,customOverlay));
     getContent().addEventListener('mouseout', makeOutListener(customOverlay));
@@ -94,8 +102,36 @@ function createPlaceObjects(jsonData) {
     */
     return places;
 
+}
 
 
+
+var activeId = null;
+var timeoutId = null;
+
+
+//커스텀오버레이를 표시하는 클로저를 만드는 함수입니다
+function makeOverListener(map,marker,customOverlay) {
+    return function(){
+        if (timeoutId !== null && i === activeId) {
+            window.clearTimeout(timeoutId);
+            timeoutId = null;
+            return;
+        }
+        customOverlay.setMap(map);
+        activeId = i;
+    }
+}
+
+// 커스텀오버레이를 닫는 클로저를 만드는 함수입니다
+function makeOutListener(customOverlay) {
+    return function(){
+        timeoutId = window.setTimeout(function() {
+            customOverlay.setMap(null);
+            activeId = null;
+            timeoutId = null;
+        }, 50);
+    }
 }
 
 
