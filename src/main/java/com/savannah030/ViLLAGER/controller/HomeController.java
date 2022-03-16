2447bbca3eba83703197ebd52951d3c7a57f5bc5
savannah030 <savannah030@naver.com>
2022-03-17 02:04:41 +0900

    Feat: HomeController `@GetMapping("/")`에서 세션에 구글 로그인한 사용자 있으면 모델에 추가하고 뷰 리턴

--- src/main/java/com/savannah030/ViLLAGER/controller/HomeController.java
+++ src/main/java/com/savannah030/ViLLAGER/controller/HomeController.java
@@ -1,17 +1,30 @@
 package com.savannah030.ViLLAGER.controller;
 
+import com.savannah030.ViLLAGER.config.auth.SessionMemberDto;
 import com.savannah030.ViLLAGER.exception.GlobalControllerAdvice;
 import com.savannah030.ViLLAGER.exception.ReturnCode;
 import com.savannah030.ViLLAGER.exception.VillagerException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        SessionMemberDto user = (SessionMemberDto) httpSession.getAttribute("memberName");
+        if (user != null ){
+            model.addAttribute("user",user);
+        }
         return "home";
     }
 
