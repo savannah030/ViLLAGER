eb43d928b783942cdfe0d1f1f1103cc0edcb153c
savannah030 <savannah030@naver.com>
2022-03-16 14:36:35 +0900

    Fix: createPlaceObjects.js 마커 띄우기 성공
    kakao.maps.Marker 객체에는 kakao.maps.LatLng 정보가 잘 들어가는데 kakao.maps.CustomOverlay에는 그렇지 않음

--- src/main/resources/static/js/kakao-map/createMap.js
+++ src/main/resources/static/js/kakao-map/createMap.js
@@ -1,9 +1,9 @@
 /////////////////////////지도///////////////////////////////////
-const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+let mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(37.48603206504228,126.98308494303069 ), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.498095,127.027610), // 지도의 중심좌표(default 강남역)
         level: 3 // 지도의 확대 레벨
     };
 
-const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 /////////////////////////////////////////////////////////////////
--- src/main/resources/static/js/kakao-map/createPlaceObjects.js
+++ src/main/resources/static/js/kakao-map/createPlaceObjects.js
@@ -8,18 +8,16 @@ function NewPlace(marker, customOverlay){
 
 //마커이미지의 주소와, 크기, 옵션으로 마커 이미지를 생성하여 리턴하는 함수입니다
 function createMarkerImage(src, size) {
-    const markerImage = new kakao.maps.MarkerImage(src, size);
-    return markerImage;
+    return new kakao.maps.MarkerImage(src, size);
 }
 
 // 좌표와 마커이미지를 받아 마커를 생성하여 리턴하는 함수입니다
 function createMarker(position, image) {
-    var marker = new kakao.maps.Marker({
+
+    return new kakao.maps.Marker({
         position: position,
         image: image
     });
-
-    return marker;
 }
 
 function createPlaceObjects(jsonData) {
@@ -32,27 +30,33 @@ function createPlaceObjects(jsonData) {
 
         json.forEach((data) => {
 
-            const idx = data.idx; //ok
-            const category = data.categoryType; //ok
-            const title = data.title; //ok
-            const content = data.content; //ok
-            const latitude = data.latitude; //ok
-            const longitude = data.longitude; //ok
+            let idx = data.idx; //ok
+            let category = data.categoryType; //ok
+            let title = data.title; //ok
+            let content = data.content; //ok
+            let latitude = data.latitude; //ok
+            let longitude = data.longitude; //ok
 
-            const location = new kakao.maps.LatLng(latitude,longitude);
+            let location = new kakao.maps.LatLng(latitude,longitude);
 
-            //const markerImage = createMarkerImage("src", new kakao.maps.Size(30,30)); //FIXME
+            let markerImage = createMarkerImage('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', new kakao.maps.Size(30,30));
+            let marker = createMarker(location, markerImage);
 
-            //marker = createMarker(location, markerImage);
-            var customOverlay = createCustomOverlay(location, idx, category, title, content);
-            console.log("customOverlay",customOverlay);
-            customOverlay.setMap(map);
-            //kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map,marker,customOverlay));
-            //kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(customOverlay));
+            let customOverlay = createCustomOverlay(location, idx, category, title, content);
+
+            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map,marker,idx,customOverlay));
+            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(customOverlay));
+
+            //console.log("getContent()",getContent()); //ok
+            getContent().addEventListener('mouseover', makeOverListener(map,marker,idx,customOverlay));
+            getContent().addEventListener('mouseout', makeOutListener(customOverlay));
+
+            let place = new NewPlace(marker, customOverlay);
+            places.push(place);
 
         });
     });
-
+    //console.log("places:",places); // ok
     return places;
 
 }
@@ -62,15 +66,15 @@ var activeId = null;
 var timeoutId = null;
 
 //커스텀오버레이를 표시하는 클로저를 만드는 함수입니다
-function makeOverListener(map,marker,customOverlay) {
+function makeOverListener(map,marker,idx,customOverlay) {
     return function(){
-        if (timeoutId !== null && i === activeId) {
+        if (timeoutId !== null && idx === activeId) {
             window.clearTimeout(timeoutId);
             timeoutId = null;
             return;
         }
         customOverlay.setMap(map);
-        activeId = i;
+        activeId = idx;
     }
 }
 
