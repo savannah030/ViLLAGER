7552413ae62bb67090f013bd0fae34f9d42c00e5
savannah030 <savannah030@naver.com>
2022-03-17 14:28:18 +0900

    comment: CustomOAuth2UserService 주석추가

--- src/main/java/com/savannah030/ViLLAGER/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/savannah030/ViLLAGER/config/auth/CustomOAuth2UserService.java
@@ -3,6 +3,7 @@ package com.savannah030.ViLLAGER.config.auth;
 import com.savannah030.ViLLAGER.domain.entity.Member;
 import com.savannah030.ViLLAGER.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -11,6 +12,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpSession;
 import java.util.Collections;
@@ -18,6 +20,7 @@ import java.util.Collections;
 // NOTE: OAuth2UserService를 구현한 CustomOAuth2UserService 클래스는
 //  구글 로그인 이후 가져온 사용자의 정보(이름, email주소, 사진)들을 기반으로
 //  가입 및 정보수정, 세션 저장 등의 기능을 지원
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -36,11 +39,10 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
          *  구글, 네이버, 카카오 등 인증 서버 구분
          *  1. registrationId : 인증 서버 id (userRequest.clientRegistration.registrationId)
          *  2. userNameAttributeKey : OAuth2 로그인 진행시 사용할 키 (userRequest.clientRegistration.providerDetails.userInfoEndpoint.userNameAttributeName)
-         *      cf) 구글의 기본코드는 "sub"
          *  3. OAuthAttributesDto : OAuth2User의 속성값 담을 클래스
          */
-        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // 1.
-        String userNameAttributeKey = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName(); //2.
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // 1. google
+        String userNameAttributeKey = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName(); //2. sub
         // NOTE: OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야함
         OAuthAttributesDto attributes = OAuthAttributesDto.of(registrationId, userNameAttributeKey, oAuth2User.getAttributes()); //3.
 
@@ -60,6 +62,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 attributes.getNameAttributeKey());
     }
 
+    //@Transactional
     private Member saveOrUpdate(OAuthAttributesDto attributes){
         Member member = memberRepository.findByEmail(attributes.getEmail())
                 // 이미 가입한 사용자이면(해당 이메일을 쓰는 사용자가 있으면) 엔티티 업데이트
