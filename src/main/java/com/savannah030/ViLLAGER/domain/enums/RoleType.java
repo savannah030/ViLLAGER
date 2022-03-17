7972debc21ae818cfa17371c6ebd38725efe0e25
savannah030 <savannah030@naver.com>
2022-03-17 14:48:25 +0900

    Comment: RoleType enum 주석 추가

--- src/main/java/com/savannah030/ViLLAGER/domain/enums/RoleType.java
+++ src/main/java/com/savannah030/ViLLAGER/domain/enums/RoleType.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RoleType {
 
+    // NOTE: 스프링 시큐리티에서는 권한 코드에 항상 ROLE_이 있어야함
     GUEST("ROLE_GUEST", "손님"),
     USER("ROLE_USER", "일반 사용자");
 
