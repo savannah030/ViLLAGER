d70f9f3cd5d86897d85308279fa76aaa7ff94802
savannah030 <savannah030@naver.com>
2022-03-18 16:50:19 +0900

    HotFix: SessionMemberDto에 Member엔티티의 idx도 추가

--- src/main/java/com/savannah030/ViLLAGER/config/auth/dto/SessionMemberDto.java
+++ src/main/java/com/savannah030/ViLLAGER/config/auth/dto/SessionMemberDto.java
@@ -8,11 +8,13 @@ import java.io.Serializable;
 @Getter
 // NOTE: 인증된 사용자의 정보만 담는 dto(직렬화)
 public class SessionMemberDto implements Serializable {
+    private Long idx;
     private String memberName;
     private String email;
     private String picture;
 
     public SessionMemberDto(Member member){
+        this.idx = member.getIdx();
         this.memberName = member.getMemberName();
         this.email = member.getEmail();
         this.picture = member.getPicture();
